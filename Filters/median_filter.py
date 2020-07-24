340a1fc0a64ba4569349457f856a2de622a951a3
Egorkassss <44812777+Egorkassss@users.noreply.github.com>
2020-07-24 18:45:50 +0300

    Pixel count
    Through the cycle, the sum of all colors is calculated, and element-wise, each color is written into a list of pix. After calculating the sum of all colors of a pixel, it is entered as the fourth element in the list of pix.

--- Filters/median_filter.py
+++ Filters/median_filter.py
@@ -8,6 +8,11 @@ def filter_median(image):
                 j = -1
                 while j != 2:
                     sum_color, elements, pix = 0, 0, []
+                    while size_image[2] != elements:
+                        sum_color += image[column + i][row + j][elements]
+                        pix.append(image[column + i][row + j][elements])
+                        elements += 1
+                    pix.append(sum_color)
                     j += 1
                 i += 1
             row += 1

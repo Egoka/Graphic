88b2141adaa0806148b9cdfcf78dfdfbd24e6b04
Egorkassss <44812777+Egorkassss@users.noreply.github.com>
2020-07-24 18:33:13 +0300

    Element-wise passage
    The image loops through each pixel. By hitting on each pixel, the algorithm is reflected to the surrounding (approximating) pixels.

--- Filters/median_filter.py
+++ Filters/median_filter.py
@@ -1,4 +1,14 @@
 def filter_median(image):
     size_image = image.shape
     column, row = 1, 1
+    while size_image[0] - 1 != column:
+        while size_image[1] - 1 != row:
+            i = -1
+            while i != 2:
+                j = -1
+                while j != 2:
+                    j += 1
+                i += 1
+            row += 1
+        column += 1
     return image

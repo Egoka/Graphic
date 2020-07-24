4743460f9ceb9f8c80c9aad46b45f256e2daabc2
Egorkassss <44812777+Egorkassss@users.noreply.github.com>
2020-07-24 19:58:12 +0300

    Declaration of input variables.
    size_image - image sizes are copied.
    n_size - The user enters the size of the window around each pixel.
    k_factor - The user enters the filter efficiency in percent.
    border - the thickness of the border around the pixel is calculated.
    k & l - coefficients from the formula.
    Then the condition of the formula is fulfilled.
    At the end, unnecessary variables are removed.

--- Filters/rank_filter.py
+++ Filters/rank_filter.py
@@ -2,4 +2,14 @@ import math
 
 
 def filter_rank(image):
+    size_image = image.shape
+    n_size = int(input('Введите размер окна (нечетное): '))
+    k_factor = int(input('Введите коэффициент выборки (от 0% до 100%):'))
+    border = math.floor(n_size / 2)
+    k = int((n_size ** 2) * (k_factor / 100))
+    if k_factor == 100:
+        l = int((n_size ** 2) - k)
+        k -= 1
+    else: l = int((n_size ** 2) - k - 1)
+    del k_factor, n_size
     return image
